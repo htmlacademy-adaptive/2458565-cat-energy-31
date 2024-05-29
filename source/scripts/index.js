b7001d4bef9f0ce84efbbbfb7e9d8d32231bb171
ANNAstasiyaBelskaya <belskayanastasiya@yandex.ru>
2024-05-29 21:26:04 +0400

    module7-task2

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,14 +2,14 @@
 const navMain = document.querySelector('.page-header__navigation');
 const navToggle = document.querySelector('.main-navigation__toggle');
 
-navMain.classList.remove('.main-nav--nojs');
+navMain.classList.remove('.main-nav__nojs');
 
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('main-nav__closed')) {
+    navMain.classList.remove('main-nav__closed');
+    navMain.classList.add('main-nav__opened');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav__closed');
+    navMain.classList.remove('main-nav__opened');
   }
 });
